077fed75f5febff8424c2a330a8e6756c5a22c19
Shravani Ghadge <shravanighadge17@gmail.com>
2022-12-25 20:37:23 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -76,7 +76,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server -> bot's response
 
-  const response = await fetch('https://codex-ijob.onrender.com/', {
+  const response = await fetch('https://codex-ijob.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type' : 'application/json'
